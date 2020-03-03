16f9007e38cae9214671b10902b31d0cb3f9da17
Filippo Finke <filippo.finke@samtrevano.ch>
2020-03-03 18:04:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# layer7-dstat 
+
+## Installation
+> npm install
+> node index.js
+
+Now you can access the dstat at http://127.0.0.1:8080
---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+const WebSocket = require('ws');
+
+const port = 8080;
+var requests = 0;
+
+const index = fs.readFileSync("./index.html");
+
+const handler = function (req, res) {
+    if (req.url == "/dstat") {
+        requests++;
+        res.end();
+    } else {
+        res.end(index);
+    }
+};
+
+const broadcast = function () {
+    wss.clients.forEach(client => client.send(requests));
+    requests = 0;
+}
+
+const server = http.createServer(handler);
+const wss = new WebSocket.Server({ server });
+
+setInterval(broadcast, 1000);
+
+console.log("Server listening on port " + port);
+server.listen(port);
