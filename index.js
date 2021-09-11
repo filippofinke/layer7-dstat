d655cb904a60106504a16cc5427f17e18d26d728
Filippo Finke <filippofinke@protonmail.ch>
2021-09-11 15:43:35 +0200

    ♻️ Remove useless JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const port = 8080;
 var requests = 0;
@@ -8,18 +8,18 @@ var requests = 0;
 const index = fs.readFileSync("./index.html");
 
 const handler = function (req, res) {
-    if (req.url == "/dstat") {
-        requests++;
-        res.end();
-    } else {
-        res.end(index);
-    }
+	if (req.url == "/dstat") {
+		requests++;
+		res.end();
+	} else {
+		res.end(index);
+	}
 };
 
 const broadcast = function () {
-    wss.clients.forEach(client => client.send(requests));
-    requests = 0;
-}
+	wss.clients.forEach((client) => client.send(requests));
+	requests = 0;
+};
 
 const server = http.createServer(handler);
 const wss = new WebSocket.Server({ server });
